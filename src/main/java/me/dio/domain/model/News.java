27293f34fe5f38abebe82266971a1e39306920d2
Yann Lima <yann_athayde@hotmail.com>
2023-11-28 21:08:52 -0300

    Primeira versão

---
+++ src/main/java/me/dio/domain/model/News.java
@@ -0,0 +1,10 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.Entity;
+import model.BaseItem;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+	
+	
+}
---
+++ src/main/java/me/dio/service/impl/UserServiceImpl.java
@@ -0,0 +1,32 @@
+package me.dio.service.impl;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.stereotype.Service;
+
+import domain.repository.UserRepository;
+import me.dio.domain.model.User;
+import me.dio.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+	
+	private final UserRepository userRepository;
+	
+	public UserServiceImpl(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@Override
+	public User findById(Long id) {
+		return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+	}
+
+	@Override
+	public User create(User userToCreate) {
+		if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+			throw new IllegalArgumentException("This User ID already exists.");
+		}
+		return userRepository.save(userToCreate);
+	}
+}
